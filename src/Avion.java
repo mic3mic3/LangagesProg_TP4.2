81d8305efb63f5d69a897c16ad97428ad59bc000
mic3mic3 <mic3mic3@hotmail.com>
2015-11-28 09:43:48 -0500

    Main temporaire

--- src/Avion.java
+++ src/Avion.java
@@ -1,4 +1,24 @@
 
 public class Avion {
-
+	// Temporaire, le temps d'ajouter toutes les classes.
+	// Aller dans Run>Run configurations>Java application et
+	// modifier la classe où se trouve le main.
+	public static void main(String[] args)
+	{
+	}
+	
+	// Propriétés
+	private int _quantiteEssence;
+	public int getQuantiteEssence()
+	{
+		return _quantiteEssence;
+	}
+	
+	public void setQuantiteEssence(int value)
+	{
+		_quantiteEssence = value;
+	}
+	
+	
+	// Méthodes
 }
