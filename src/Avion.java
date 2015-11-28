dc960a36627b38180eda25bc71ed2e026f8b8460
mic3mic3 <mic3mic3@hotmail.com>
2015-11-28 10:06:17 -0500

    Avion abstract

--- src/Avion.java
+++ src/Avion.java
@@ -1,5 +1,5 @@
 
-public class Avion {
+public abstract class Avion {
 	// Temporaire, le temps d'ajouter toutes les classes.
 	// Aller dans Run>Run configurations>Java application et
 	// modifier la classe où se trouve le main.
